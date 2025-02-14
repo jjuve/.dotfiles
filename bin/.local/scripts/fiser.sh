3ce9186c2a9ddc74b77654d1bc04f5e655ee6374
jjuve <75339345+jjuve@users.noreply.github.com>
2025-02-14 11:59:08 -0600

    updated fiser script

--- bin/.local/scripts/fiser.sh
+++ bin/.local/scripts/fiser.sh
@@ -1,19 +1,39 @@
 #!/usr/bin/env bash
 
-directory="/Users/jj/app"
+DIRECTORY="/Users/jj/app"
 
 function iterate(){
 	local dir="$1"
 
 	for x in "$dir"/*; do
 		if [[ -f "$x" ]]; then
-			echo "$x"
-		fi
-		if [[ -d "$x" ]]; then
+			case $x in
+				 *.png )
+				echo "Copying $x"
+				cp $x /Users/jj/tmp/png
+				;;
+				*)
+				;;
+			esac
+		elif [[ -d "$x" ]]; then
 			echo "$x"
 			iterate "$x"
 		fi
 	done
 }
 
-iterate "$directory"
+if [[ -d /Users/jj/tmp ]]; then
+	echo "Directory found"
+	iterate "$DIRECTORY"
+elif [[ false ]]; then
+	echo "Directory not found, making directories"
+	directories=("/Users/jj/tmp/pdf" "/Users/jj/tmp/jpeg")
+
+	for directory in "${directories[@]}";do
+	mkdir -v -p "$directory"
+	done
+
+	iterate "$DIRECTORY"
+fi
+
+echo "==================== Script Done ===================="
