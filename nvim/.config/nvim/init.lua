998f7e5e0e017a3a73b0ef5b38e7e04752306237
jjuve <75339345+jjuve@users.noreply.github.com>
2025-09-05 16:51:44 -0500

    updated nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -42,7 +42,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local lazyrepo = "git@github.com:folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
@@ -57,7 +57,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugin = {
-  {"folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {},},
+  {"git@github.com:folke/tokyonight.nvim.git", lazy = false, priority = 1000, opts = {},},
 }
 
 local opts = {}
