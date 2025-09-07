98d0d448f9b04ae72ab6649ae27f8af3342b2866
jjuve <75339345+jjuve@users.noreply.github.com>
2025-09-06 22:03:00 -0500

    updated nvim

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return{
+{
+    'git@github.com:nvim-lualine/lualine.nvim.git',
+    dependencies = { 'git@github.com:nvim-tree/nvim-web-devicons.git',
+		     'git@github.com:nvim-tree/nvim-tree.lua.git',
+    },
+    opts = {
+	options = {
+	    theme = 'tokyonight',
+	} 
+    }
+}
+} 
