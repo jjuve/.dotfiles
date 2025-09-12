cdd5a964f17576c8c6c103231491807b580c3205
jjuve <75339345+jjuve@users.noreply.github.com>
2025-09-12 14:11:42 -0500

    updated nvim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,16 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  dependencies = { 'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
   config = function()
+    require('telescope').setup {
+      extensions = {
+        fzf = {}
+      }
+    }
+
     local builtin = require('telescope.builtin')
 
     require('telescope').load_extension('fzf')
